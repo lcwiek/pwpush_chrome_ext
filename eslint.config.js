14c3d423abfe40fabd550d573a2712800b0628e9
l.cwiek <l.cwiek@codinglifestyle.pl>
2025-04-10 20:55:41 +0200

    Build init

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+      files: ['**/*.js'],
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      rules: {
+        semi: ['error', 'always'],
+        quotes: ['error', 'single']
+      }
+    }
+  ];
+  
